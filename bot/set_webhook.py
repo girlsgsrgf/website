31d2f8ec54bc215c711ed397b26728f5ee994edb
kalilinux <eric@gmail.com>
2025-06-04 16:09:19 +0500

    last

--- bot/set_webhook.py
+++
@@ -1,8 +0,0 @@
-# set_webhook.py
-import requests
-
-TOKEN = "8155139765:AAHUTF9FcQ7SYqpDRMFR1VaCtcxyhl3F2Vg"
-WEBHOOK_URL = "https://flyup.help/webhook/"
-
-res = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}")
-print(res.json())
