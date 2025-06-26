e86a098c51c5177b6e4c061c8fc5b3d9b2717b7b
kalilinux <eric@gmail.com>
2025-06-26 17:18:56 +0500

    GOD

--- frontend/src/MarketplacePage.js
+++ frontend/src/MarketplacePage.js
@@ -82,7 +82,20 @@ function MarketPlacePage() {
             </div>
           ))
         ) : (
-          <p className="no-products">No products found</p>
+          <div class="realloader">
+              <div class="bar1"></div>
+              <div class="bar2"></div>
+              <div class="bar3"></div>
+              <div class="bar4"></div>
+              <div class="bar5"></div>
+              <div class="bar6"></div>
+              <div class="bar7"></div>
+              <div class="bar8"></div>
+              <div class="bar9"></div>
+              <div class="bar10"></div>
+              <div class="bar11"></div>
+              <div class="bar12"></div>
+          </div>
         )}
       </div>
     </div>
